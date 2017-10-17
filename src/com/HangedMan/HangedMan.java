525cab0297ffb67ed6119034f0adca18bf695724
christasca <32681592+christasca@users.noreply.github.com>
2017-10-17 18:22:36 +0200

    controlli
    modificato controllo lettere e vittoria.

--- src/com/HangedMan/HangedMan.java
+++ src/com/HangedMan/HangedMan.java
@@ -1,5 +1,3 @@
-
-
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -22,9 +20,33 @@ public class HangedMan {
 		boolean isWin = false;
 
 		secretWord = insertSecretWord();
+		
 
 		int lengthWord = secretWord.length();
+		System.out.print("La lunghezza della parola da trovare e': " + lengthWord + "\nLa parola da trovare: ");
+		
+		int length = lengthWord-1;
+		
+		
+		
+		
 		char[] word = new char[lengthWord];
+		String word1[] = new String[lengthWord];
+		int index;
+		String word2;
+		//faccio vedere quale è la parola, con la lettera iniziale e la lettera finale;
+		for (int i = 0; i <= length; i++){
+			if (i==0 || i == length){
+				System.out.print(secretWord.charAt(i));
+				word[i] = secretWord.charAt(i);
+				
+			}
+			else {
+				System.out.print("_");
+			word[i] = '_';}
+			
+				
+		}
 		
 		//Turno di gioco
 		while(errors <= MAX_ERRORS && !isWin) {
@@ -41,27 +63,42 @@ public class HangedMan {
 			
 			//Controllo occorenze tra lettera inserita e la parola segreta
 			if(secretWord.contains(insertedChar)) {
-				System.out.println("Bravo la lettera " + insertedChar + " è contenuta la parola segreta");
-				int index = secretWord.indexOf(insertedChar);
-				word[index] = insertedChar.charAt(0);
+				System.out.println("Bravo la lettera " + insertedChar + " e' contenuta nella parola segreta");
+				word1[0]= "";
+			
+			// inserisco la lettera, all'interno di word, e t parte da 1 e length-1 perchè la prima lettera e l'ultima non devo contarle.
+			for(int t = 1; t<=length-1; t++){
+				if(insertedChar.charAt(0) == secretWord.charAt(t)){
+					word[t] = insertedChar.charAt(0);
+				}
+			}
+			for(int o = 0; o <= length; o++){
+			
+				word1[0] = word1[0] + word[o];
+			}
 
 			}else{
-				System.out.println("La lettera " + insertedChar + "  non è contenuta la parola segreta");
+				System.out.println("La lettera " + insertedChar + "  non e' contenuta nella parola segreta");
 				errors++;
 			}
 
-			//Controllo vittoria
-			if(secretWord.equals(word)){
-				isWin = true;
-			}
+			//Controllo vittoria ....
 			
+			word2 = word1[0];
+			if(secretWord.equals(word2)){
+				isWin = true;
+				System.out.println("Hai Vinto!");
+				System.out.println("la parola finale e': " + word2);
+				System.exit(0);	
+			}
+				System.out.println("la parola trovata fin'ora e' " + word1[0]);
 			printHangedMan(errors,word,lengthWord);
 			
 		}
 		
 		System.out.println("Fine, hai perso!!!");
 		System.exit(0);		
-		
+	
 	}
 	
 	/*
@@ -102,12 +139,12 @@ public class HangedMan {
 		}
 
 		
-		//NON RIESCO A MOSTRARE LE PAROLE TROVATE
-		for(int x = 0; x < lengthWord;++x){
-			if(word[x] == ' '){
+	
+		for(int i = 0; i < lengthWord; i++){
+			if(word[i] == ' '){
 				System.out.print(" _ ");
 			}else{
-				System.out.print(word[x]);
+				System.out.print(word[i]);
 			}
 		}
 		
